a41a59dd864d9f252d19756392bdc2fa94c07b64
tuytmeiii <le0827596@gmail.com>
2023-11-09 23:15:08 +0700

    bai tap buoi 2

---
+++ bt1.php
@@ -0,0 +1,10 @@
+<!DOCTYPE 
+<html>
+    <body>
+        <?php
+        ECHO "hello world! <br>";
+        echo "hello world! <br>";
+        Echo "hello world! <br>";
+        ?>
+    </body>
+</html>
